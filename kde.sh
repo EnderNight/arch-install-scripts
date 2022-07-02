822e67673c4caa275c3aca973ee08667b997b8a7
Ender <ndxendernight@gmail.com>
2022-07-02 23:17:10 +0200

    Finished kde.sh

--- kde.sh
+++ kde.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# Base packages
+yay -S plasma kde-applications materia-kde papirus-icon-theme
 
+# Display manager
+systemctl enable sddm
 
+# More settings can be setup after restarting...
+
+printf "\e[0:31mDone ! Restarting in 5 sec...\e[m"
+sleep 5
+reboot

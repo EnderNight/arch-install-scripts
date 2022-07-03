80ede66ead126a284fbf3169d888fd8168bfb5e6
EnderNight <66211913+EnderNight@users.noreply.github.com>
2022-07-03 14:30:24 +0200

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Base packages
-yay -S plasma kde-applications materia-kde papirus-icon-theme code unityhub gimp inkscape chromium discord audacity lmms vlc obs-studio libreoffice-fresh ttf-gentium-basic adobe-source-code-pro-fonts adobe-source-sans-fonts adobe-source-serif-fonts hunspell hunspell-fr hunspell-en_us hyphen hyphen-fr libmythes mythes-fr libreoffice-extension-languagetool jre8-openjdk libreoffice-extension-grammalecte-fr openrazer-meta polychromatic polymc lutris steam legendary zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions zsh-theme-powerlevel10k ttf-meslo-nerd-font-powerlevel10k cmake nodejs jdk11-openjdk npm mono grapejuice wget nordic-wallpapers materia-gtk-theme kvantum-theme-materia
+yay -S plasma kde-applications materia-kde papirus-icon-theme code unityhub gimp inkscape chromium discord audacity lmms vlc obs-studio libreoffice-fresh ttf-gentium-basic adobe-source-code-pro-fonts adobe-source-sans-fonts adobe-source-serif-fonts hunspell hunspell-fr hunspell-en_us hyphen hyphen-fr libmythes mythes-fr libreoffice-extension-languagetool jdk8-openjdk libreoffice-extension-grammalecte-fr openrazer-meta polychromatic polymc lutris steam legendary zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions zsh-theme-powerlevel10k ttf-meslo-nerd-font-powerlevel10k cmake nodejs npm mono grapejuice wget nordic-wallpapers materia-gtk-theme kvantum-theme-materia
 
 # Display manager
 systemctl enable sddm

146ad42485acd0501df7cb09069a2969b2f618c3
Ender <ndxendernight@gmail.com>
2022-07-03 11:33:48 +0200

    Added spacevim and mmck laucher

--- kde.sh
+++ kde.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
 # Base packages
-yay -S --needed plasma kde-applications materia-kde papirus-icon-theme code unityhub gimp inkscape chromium discord audacity lmms vlc obs-studio libreoffice-fresh ttf-gentium-basic adobe-source-code-pro-fonts adobe-source-sans-fonts adobe-source-serif-fonts hunspell hunspell-fr hunspell-en_us hyphen hyphen-fr libmythes mythes-fr libreoffice-extension-languagetool jre8-openjdk libreoffice-extension-grammalecte-fr openrazer-meta polychromatic linux-zen linux-zen-headers polymc lutris steam legendary zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions zsh-theme-powerlevel10k ttf-meslo-nerd-font-powerlevel10k cmake nodejs jdk11-openjdk npm mono grapejuice
+yay -S --needed plasma kde-applications materia-kde papirus-icon-theme code unityhub gimp inkscape chromium discord audacity lmms vlc obs-studio libreoffice-fresh ttf-gentium-basic adobe-source-code-pro-fonts adobe-source-sans-fonts adobe-source-serif-fonts hunspell hunspell-fr hunspell-en_us hyphen hyphen-fr libmythes mythes-fr libreoffice-extension-languagetool jre8-openjdk libreoffice-extension-grammalecte-fr openrazer-meta polychromatic linux-zen linux-zen-headers polymc lutris steam legendary zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions zsh-theme-powerlevel10k ttf-meslo-nerd-font-powerlevel10k cmake nodejs jdk11-openjdk npm mono grapejuice wget
 
-## Display manager
-#systemctl enable sddm
-#
-## Open razer
-#sudo gpasswd -a ender plugdev
-#
-## Regenerating grub config
-#sudo grub-mkconfig -o /boot/grub/grub.cfg
-#
-## Zsh
-#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#echo "source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme" >> ~/.zshrc
-#echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-#echo "source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
-#sed -i 's/plugins=(git)/plugins=(git archlinux)/' ~/.zshrc
-#
-## Vim
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-#cp .vimrc ~/.vimrc
-#vim +PluginInstall +qall
-#python3 ~/.vim/bundle/youcompleteme/install.py --all
-#
-## More settings can be setup after restarting...
-#
-#printf "\e[0:31mDone ! Restarting in 5 sec...\e[m"
-#sleep 5
-#reboot
+# Display manager
+systemctl enable sddm
+
+# Open razer
+sudo gpasswd -a ender plugdev
+
+# Regenerating grub config
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+# Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme" >> ~/.zshrc
+echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+echo "source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+sed -i 's/plugins=(git)/plugins=(git archlinux)/' ~/.zshrc
+
+# Vim
+curl -sLf https://spacevim.org/install.sh | bash
+vim
+
+# MMcK Launcher
+wget https://github.com/SnoutBug/mmcklauncher/releases/download/0.6/com.github.SnoutBug.mmckLauncher.tar.gz
+plasmapkg2 --install com.github.SnoutBug.mmckLauncher.tar.gz
+rm com.github.SnoutBug.mmckLauncher.tar.gz
+
+# More settings can be setup after restarting...
+
+printf "\e[0:31mDone ! Restarting in 5 sec...\e[m"
+sleep 5
+reboot
